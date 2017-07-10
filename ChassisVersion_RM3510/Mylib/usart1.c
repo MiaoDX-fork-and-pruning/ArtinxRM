fbc1c245ff900565a0349ec3f5454dc844494455
miaodx <miaodx@hotmail.com>
2017-07-10 20:20:46 +0800

    Change DBUS receiver according to [mobangjack/RM2016CampCollegeInfantry](https://github.com/mobangjack/RM2016CampCollegeInfantry.git) by using two MDA.

--- ChassisVersion_RM3510/Mylib/usart1.c
+++ ChassisVersion_RM3510/Mylib/usart1.c
@@ -15,78 +15,111 @@ No   Version    Date     Revised By       Item       Description
 1     1.1       6/28       yf   			usart1ÅäÖÃº¯Êý ÅäÖÃgpio¡¢usart³õÊ¼»¯¡¢dmaºÍnvic		
 2     1.2       6/29       gyf 
 3     1.3       6/29       yf 					  ×¢ÊÍ			   
+4			1.4				7/10			 miaodx			Change DBUS receiver according to [mobangjack/RM2016CampCollegeInfantry](https://github.com/mobangjack/RM2016CampCollegeInfantry.git)
 ************************************************************************************/
 #include "main.h"
 
-
-unsigned char dbus_buf[DBUS_BUF_SIZE];
+uint8_t usart1_dma_rx_buf[2][USART1_DMA_RX_BUF_SIZE];
+uint8_t* dbuf = usart1_dma_rx_buf[0];
+DBUS dbus;
 
 void USART1_Config(void)
 {
-    USART_InitTypeDef usart1;
-    GPIO_InitTypeDef  gpio;
-    DMA_InitTypeDef   dma;
+    GPIO_InitTypeDef gpio;
+	USART_InitTypeDef usart;
+	NVIC_InitTypeDef nvic;
+    DMA_InitTypeDef dma;
     
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB | RCC_AHB1Periph_DMA2,ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE); 
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE); 
     
-    GPIO_PinAFConfig(GPIOB,GPIO_PinSource7 ,GPIO_AF_USART1);
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_USART1);
     
-    gpio.GPIO_Pin = GPIO_Pin_7 ;
+    GPIO_StructInit(&gpio);
+    gpio.GPIO_Pin = GPIO_Pin_7;
     gpio.GPIO_Mode = GPIO_Mode_AF;
-    gpio.GPIO_OType = GPIO_OType_PP;
-    gpio.GPIO_Speed = GPIO_Speed_100MHz;
-    gpio.GPIO_PuPd = GPIO_PuPd_NOPULL;
-    GPIO_Init(GPIOB,&gpio);
+    gpio.GPIO_Speed = GPIO_Speed_2MHz;
+    gpio.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_Init(GPIOB, &gpio);
     
     USART_DeInit(USART1);
-    usart1.USART_BaudRate = 100000;   //SBUS 100K baudrate
-    usart1.USART_WordLength = USART_WordLength_8b;
-    usart1.USART_StopBits = USART_StopBits_1;
-    usart1.USART_Parity = USART_Parity_Even;
-    usart1.USART_Mode = USART_Mode_Rx;
-    usart1.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART_Init(USART1,&usart1);
+    USART_StructInit(&usart);
+    usart.USART_BaudRate = 100000;
+    usart.USART_WordLength = USART_WordLength_8b;
+    usart.USART_StopBits = USART_StopBits_1;
+    usart.USART_Parity = USART_Parity_Even;
+    usart.USART_Mode = USART_Mode_Rx;
+    usart.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_Init(USART1, &usart);
     
-    USART_Cmd(USART1,ENABLE);
-    USART_DMACmd(USART1,USART_DMAReq_Rx,ENABLE);
+    USART_DMACmd(USART1, USART_DMAReq_Rx, ENABLE);
     
-		NVIC_Set(DMA2_Channel,DMA2_PreemptionPriority,DMA2_SubPriority,ENABLE);
-    
-    DMA_DeInit(DMA2_Stream5);
-    dma.DMA_Channel= DMA_Channel_4;
-    dma.DMA_PeripheralBaseAddr = (u32)&(USART1->DR);
-    dma.DMA_Memory0BaseAddr = (u32)dbus_buf;
+    DMA_DeInit(DMA2_Stream2);
+    DMA_StructInit(&dma);
+    dma.DMA_Channel = DMA_Channel_4;
+    dma.DMA_PeripheralBaseAddr = (uint32_t)(&USART1->DR);
+    dma.DMA_Memory0BaseAddr = (uint32_t)&usart1_dma_rx_buf[0][0];
     dma.DMA_DIR = DMA_DIR_PeripheralToMemory;
-    dma.DMA_BufferSize = DBUS_BUF_SIZE;
+    dma.DMA_BufferSize = sizeof(usart1_dma_rx_buf)/2;
     dma.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
     dma.DMA_MemoryInc = DMA_MemoryInc_Enable;
     dma.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
     dma.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
     dma.DMA_Mode = DMA_Mode_Circular;
-    dma.DMA_Priority = DMA_Priority_VeryHigh;
+    dma.DMA_Priority = DMA_Priority_Medium;
     dma.DMA_FIFOMode = DMA_FIFOMode_Disable;
     dma.DMA_FIFOThreshold = DMA_FIFOThreshold_1QuarterFull;
-    dma.DMA_MemoryBurst = DMA_Mode_Normal;
+    dma.DMA_MemoryBurst = DMA_MemoryBurst_Single;
     dma.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
-    DMA_Init(DMA2_Stream5,&dma);
+    DMA_Init(DMA2_Stream2, &dma);
+    
+    DMA_DoubleBufferModeConfig(DMA2_Stream2, (uint32_t)&usart1_dma_rx_buf[1][0], DMA_Memory_0);   //first used memory configuration
+    DMA_DoubleBufferModeCmd(DMA2_Stream2, ENABLE);
+    
+    DMA_Cmd(DMA2_Stream2, ENABLE);
+    
+    nvic.NVIC_IRQChannel = USART1_IRQn;                          
+	nvic.NVIC_IRQChannelPreemptionPriority = 0;
+	nvic.NVIC_IRQChannelSubPriority = 0;		    
+	nvic.NVIC_IRQChannelCmd = ENABLE;			
+	NVIC_Init(&nvic);	
+    
+	USART_ITConfig(USART1, USART_IT_IDLE, ENABLE); // 14mspf
+    USART_Cmd(USART1, ENABLE);
 
-    DMA_ITConfig(DMA2_Stream5,DMA_IT_TC,ENABLE);
-    DMA_Cmd(DMA2_Stream5,ENABLE);
 }
-DBUS dbus;
-void DMA2_Stream5_IRQHandler(void)
+
+void USART1_IRQHandler(void)
 {
-    if(DMA_GetITStatus(DMA2_Stream5, DMA_IT_TCIF5))
-    {
-       
-			
-        DMA_ClearFlag(DMA2_Stream5, DMA_FLAG_TCIF5);
-        DMA_ClearITPendingBit(DMA2_Stream5, DMA_IT_TCIF5);
+	static uint32_t this_time_rx_len = 0;
+	if(USART_GetITStatus(USART1, USART_IT_IDLE) != RESET)
+	{
+		//clear the idle pending flag 
+		(void)USART1->SR;
+		(void)USART1->DR;
 
-        /*******************decode DBUS data*******************/
-				// printf("DBUS_Dec"); // IT SEEMS THAT ONLY WHEN CONNECTED THE RC, THIS WILL BE CALLED
-        DBUS_Dec(&dbus,dbus_buf);
-			
+		DMA_Cmd(DMA2_Stream2, DISABLE);
+		this_time_rx_len = USART1_DMA_RX_BUF_SIZE - DMA_GetCurrDataCounter(DMA2_Stream2);
+		DMA2_Stream2->NDTR = (uint16_t)USART1_DMA_RX_BUF_SIZE;     //relocate the dma memory pointer to the beginning position
+		//Target is Memory0
+		if(DMA_GetCurrentMemoryTarget(DMA2_Stream2) == 0)
+		{
+			dbuf = usart1_dma_rx_buf[0];
+			DMA2_Stream2->CR |= (uint32_t)(DMA_SxCR_CT);        //enable the current selected memory is Memory 1
 		}
+		else
+		{
+			dbuf = usart1_dma_rx_buf[1];
+			DMA2_Stream2->CR &= ~(uint32_t)(DMA_SxCR_CT);       //enable the current selected memory is Memory 0
+		}
+		DMA_Cmd(DMA2_Stream2, ENABLE);
+		if(this_time_rx_len == RC_FRAME_LEN)
+		{
+			DBUS_Dec(&dbus,dbuf);
+			//DBUS_DEC(&dbus, dbuf);
+			//InputTask();
+		}
+	}       
 }
+
